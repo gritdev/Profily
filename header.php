8700c357c188f99979cba065b730bfd040b9514a
vikramgrewal <vik.grewal1992@gmail.com>
2015-09-25 20:01:17 -0700

    Updated header file

--- header.php
+++ header.php
@@ -7,12 +7,27 @@
 
 <?php
     ob_start();
+    include 'bootstrap.php';
 ?>
 
 
 
-    <div class="navbar navbar-fixed-top">
-        <form  class="input-group col-sm-6 col-sm-push-3" role="form" method="post" action="browse.php">
+    <nav class="navbar navbar-fixed-top">
+        <!-- This will contain the search button icon for displaying the search bar in responsive
+        form -->
+        <div class="visible-xs col-xs-2">
+            
+                <span class="glyphicon glyphicon-search" id="search container"></span>
+        
+        </div>
+
+        <!-- This is where the logo will be placed -->
+        <div class="Brand col-sm-3 col-xs-6">
+            <a href="index.php"><h1>Profily.me</h1></a>
+        </div>
+        
+        <!-- Container for form will be right here -->
+        <form  class="input-group col-sm-5 col-xs-12" role="form" method="post" action="browse.php">
             <!-- Inputting search function for email, first name, last name, and username -->
             <input class="form-control" type="text" id="formGroupInputSmall" 
             placeholder="Enter username or email" name="searchquery">
@@ -21,12 +36,13 @@
                 <button class="btn btn-success btn-group-justified" type ="submit" name="searchfront" value="search">Search</button>
             </span>   
         </form>
-    </div>
+
+    </nav>
     
 
     
 
-</div>        
+       
 
 <!-- Everything below this and between the next comment belongs to the navbar -->
 
